15ec582f36bbbd13de83309b167d4b87d344ea24
samuelbounan <s.bounan@laposte.net>
2022-06-17 16:19:30 +0200

    graph.py

---
+++ sav/graph.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+L = []
+
+with open('result.txt') as f:
+    for line in f:
+        L.append(int(line))
+    f.close()
+
+ds = 5
+X = np.arange(0, 162, ds)
+Y = np.zeros(len(X))
+
+for el in L:
+    Y[el//ds] += 1
+
+plt.plot(X, Y)
+plt.show()
